916b9be99c142f769ae8a04a61e4cadb1bc8fbce
Gian Franco <gperalta@funciton.com>
2025-02-11 19:30:53 -0500

    feat: add header and responsive

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  devToolbar: {
+    enabled: false,
+  },
   env: {
     schema: {
       GOOGLE_PRIVATE_KEY: envField.string({
